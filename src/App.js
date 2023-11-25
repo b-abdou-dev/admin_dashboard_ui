869bcb4b11043a9c2a2e61b717d35e24f59b6640
medo-dev <55310512+b-abdou-dev@users.noreply.github.com>
2023-11-25 17:17:02 +0100

    Update App.js changed "import ./app.css/" to " import "./App.css"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import SideBar from "./components/sidebar/SideBar";
 import TopBar from "./components/topbar/TopBar";
-import "./app.css";
+import "./App.css";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
